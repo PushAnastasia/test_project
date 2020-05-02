7ce4326b8dd810fa6046c57412cf1b63058135e3
PushAnastasia <asya.push@gmail.com>
2020-05-02 18:22:05 +0300

    refactored test_createContact

--- test_createContact.py
+++ test_createContact.py
@@ -12,24 +12,34 @@ class TestCreateContact():
     self.driver.quit()
   
   def test_createContact(self):
-    self.driver.get("https://skyt.qa.sharp.nixdev.co/")
-    self.driver.set_window_size(1936, 1056)
-    self.driver.find_element(By.LINK_TEXT, "Login").click()
-    self.driver.find_element(By.ID, "email-address").click()
-    self.driver.find_element(By.ID, "email-address").send_keys("test0001push@gmail.com")
-    self.driver.find_element(By.CSS_SELECTOR, ".loginWrap > .accountcontrol__content").click()
-    self.driver.find_element(By.CSS_SELECTOR, ".loginWrap > .accountcontrol__content").send_keys("plokijuh1")
-    self.driver.find_element(By.ID, "btnLogin").click()
-    element = self.driver.find_element(By.CSS_SELECTOR, ".create-block .link-content")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element).perform()
-    self.driver.find_element(By.CSS_SELECTOR, ".create-block .link-content").click()
-    element = self.driver.find_element(By.CSS_SELECTOR, "body")
-    actions = ActionChains(self.driver)
-    actions.move_to_element(element, 0, 0).perform()
-    self.driver.find_element(By.LINK_TEXT, "Contact").click()
+    self.open_home_page()
+    self.login()
+    self.open_add_contact_page()
+    self.create_contact()
+
+  def create_contact(self):
+    # fill Contact form
     self.driver.find_element(By.ID, "CompanyName").click()
     self.driver.find_element(By.ID, "CompanyName").send_keys("TestContact")
     self.driver.find_element(By.CSS_SELECTOR, ".column:nth-child(1) > .grey-block").click()
+    # submit creation
     self.driver.find_element(By.CSS_SELECTOR, ".btn-save").click()
+
+  def open_add_contact_page(self):
+    self.driver.find_element(By.CSS_SELECTOR, ".create-block .link-content").click()
+    self.driver.find_element(By.LINK_TEXT, "Contact").click()
+
+  def login(self):
+
+    self.driver.find_element(By.LINK_TEXT, "Login").click()
+    self.driver.find_element(By.ID, "email-address").click()
+    self.driver.find_element(By.ID, "email-address").send_keys("test0001push@gmail.com")
+    self.driver.find_element(By.ID, "cpassword").click()
+    self.driver.find_element(By.ID, "cpassword").send_keys("plokijuh1")
+    self.driver.find_element(By.ID, "btnLogin").click()
+
+  def open_home_page(self):
+
+    self.driver.get("https://skyt.qa.sharp.nixdev.co/")
+    self.driver.set_window_size(1936, 1056)
   
