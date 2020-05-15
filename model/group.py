c7ed229ed28bec26427f0555a5f9879e06e78318
PushAnastasia <asya.push@gmail.com>
2020-05-16 00:18:31 +0300

    added assert

--- model/group.py
+++ model/group.py
@@ -5,3 +5,9 @@ class Group:
     def __init__(self, name=None, id=None):
         self.name = name
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.name)
+
+    def __eq__(self, other):
+        return self.name == other.name
--- test/test_inactive_contact.py
+++ test/test_inactive_contact.py
@@ -7,4 +7,5 @@ def test_inactive_first_contact(app):
     app.group.inactive_first_contact()
     new_contacts = app.group.get_contact_list()
     assert len(old_contacts) - 1 == len(new_contacts)
-    
+    old_contacts[0:1] = []
+    assert old_contacts == new_contacts
