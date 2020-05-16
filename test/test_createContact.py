a0c7c4995677e587ab2af1914df64f60623cca46
PushAnastasia <asya.push@gmail.com>
2020-05-16 23:33:48 +0300

    added assert

--- test/test_createContact.py
+++ test/test_createContact.py
@@ -4,13 +4,19 @@ from model.group import Group
 
 def test_createContact(app):
   old_contacts = app.group.get_contact_list()
-  app.group.create(Group(name="TestContact"))
+  contact = Group(name="TestContact")
+  app.group.create(contact)
   new_contacts = app.group.get_contact_list()
   assert len(old_contacts) + 1 == len(new_contacts)
+  old_contacts.append(contact)
+  assert old_contacts == new_contacts
 
 def test_createContactClient(app):
   old_contacts = app.group.get_contact_list()
-  app.group.create(Group(name="TestClient"))
+  contact = Group(name="TestClient")
+  app.group.create(contact)
   new_contacts = app.group.get_contact_list()
   assert len(old_contacts) + 1 == len(new_contacts)
+  old_contacts.append(contact)
+#  assert sorted(old_contacts) == sorted(new_contacts)
 
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,6 +2,9 @@ from model.group import Group
 
 def test_modify_contact_name(app):
   old_contacts = app.group.get_contact_list()
-  app.group.modify_first_contact(Group(name="ModifiedContact"))
+  contact = Group(name="ModifiedContact")
+  app.group.modify_first_contact(contact)
   new_contacts = app.group.get_contact_list()
   assert len(old_contacts)  == len(new_contacts)
+  old_contacts[0] = contact
+  assert old_contacts == new_contacts
