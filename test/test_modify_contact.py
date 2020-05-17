18e382c66beca956888005425f7fc0c0aa0de51b
PushAnastasia <asya.push@gmail.com>
2020-05-17 17:30:39 +0300

    fixed test

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -6,7 +6,7 @@ def test_modify_contact_name(app):
     app.group.create(Group(name="TestClient"))
   old_contacts = app.group.get_contact_list()
   index = randrange(len(old_contacts))
-  contact = Group(name="ModifiedContact")
+  contact = Group(name="TestClient")
   app.group.modify_contact_by_index(index, contact)
   new_contacts = app.group.get_contact_list()
   assert len(old_contacts) == len(new_contacts)
