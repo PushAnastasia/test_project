57ba2b011d0978b130a06f414256ced62b731634
PushAnastasia <asya.push@gmail.com>
2020-06-08 00:27:48 +0300

    edited generator

--- test/test_createContact.py
+++ test/test_createContact.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-import pytest
-from data.add_contact import constant as testdata
 
-@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_createContact(app, group):
+
+
+def test_createContact(app, data_contacts):
+       group = data_contacts
        old_contacts = app.group.get_contact_list()
        app.group.create(group)
        assert len(old_contacts) + 1 == app.group.count()
