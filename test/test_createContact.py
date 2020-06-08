1e4c45c59ef86a1d4f6bd73f1ebdce41c0a5d9bd
PushAnastasia <asya.push@gmail.com>
2020-06-08 16:08:04 +0300

    added groups json

--- test/test_createContact.py
+++ test/test_createContact.py
@@ -3,8 +3,8 @@ from model.group import Group
 
 
 
-def test_createContact(app, data_contacts):
-       group = data_contacts
+def test_createContact(app, json_groups):
+       group = json_groups
        old_contacts = app.group.get_contact_list()
        app.group.create(group)
        assert len(old_contacts) + 1 == app.group.count()
