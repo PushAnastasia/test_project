6274beb9967d69660d703ad14fc3761180f36d48
PushAnastasia <asya.push@gmail.com>
2020-06-11 23:53:46 +0300

    added optional validation

--- test/test_inactive_contact.py
+++ test/test_inactive_contact.py
@@ -1,7 +1,7 @@
 from model.group import Group
 from random import randrange
 
-def test_inactive_some_contact(app):
+def test_inactive_some_contact(app, check_ui):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_contacts = app.group.get_contact_list()
@@ -11,3 +11,5 @@ def test_inactive_some_contact(app):
     assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts[index:index+1] = []
     assert old_contacts == new_contacts
+    if check_ui:
+        assert new_contacts == app.group.get_contact_list()
